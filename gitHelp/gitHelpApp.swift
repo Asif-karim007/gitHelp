2a0dadcb05563e739795999a7d45892f068f69d0
Asif Karim <asif0171797@gmail.com>
2022-03-16 19:30:53 +0600

    First

---
+++ gitHelp/gitHelpApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitHelpApp.swift
+//  gitHelp
+//
+//  Created by Asif Karim on 3/16/22.
+//
+
+import SwiftUI
+
+@main
+struct gitHelpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
